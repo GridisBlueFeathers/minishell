4d56248a29736f9caae1a96a0fb1bc8240c031d7
CottonKiwii <julia.wolfram@gmx.at>
2024-11-15 16:30:57 +0100

    feat(data): added data singleton

--- include/minishell.h
+++ include/minishell.h
@@ -6,9 +6,25 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:24:55 by svereten          #+#    #+#             */
-/*   Updated: 2024/11/05 16:25:16 by svereten         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:23:51 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include "libft/libft.h"
+
+typedef enum e_option
+{
+	GET,
+	FREE
+}	t_option;
+
+typedef struct s_data
+{
+	int	nbr;
+}	t_data;
+
+t_data	*data(t_option option);
+
 #endif
