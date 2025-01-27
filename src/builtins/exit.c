42cf0eba6b0c24f2a7a4660d465718f2920538d8
Serhii Veretennikov <gridis72@gmail.com>
2025-01-27 12:55:50 +0100

    fix: added protection to exit builtin

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 18:33:24 by svereten          #+#    #+#             */
-/*   Updated: 2025/01/24 19:01:24 by svereten         ###   ########.fr       */
+/*   Updated: 2025/01/27 12:55:25 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/ctype.h"
@@ -29,6 +29,8 @@ void	builtin_exit(char **args)
 		minishell_exit(ft_atoi(args[1]), NULL);
 	len = 17 + ft_strlen(args[1]) + 27;
 	err_msg = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!err_msg)
+		minishell_exit(1, NULL);
 	ft_strlcat(err_msg, "minishell: exit:", len + 1);
 	ft_strlcat(err_msg, args[1], len + 1);
 	ft_strlcat(err_msg, " numeric argument required\n", len + 1);
