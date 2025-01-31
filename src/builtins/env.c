01d43cc0ad168b6d5f69fee8755be4f865176cf6
GridisBlueFeathers <gridis72@gmail.com>
2025-01-31 13:45:37 +0100

    fix: made env builtin norm compliant

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 13:14:40 by svereten          #+#    #+#             */
-/*   Updated: 2025/01/31 13:30:08 by svereten         ###   ########.fr       */
+/*   Updated: 2025/01/31 13:41:47 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/string.h"
@@ -15,21 +15,12 @@
 
 /**
  * For each node len + 2 for '=' and '\n'
- * Why allocating a separate string for output while `printf` is availible?
- * `printf` might fail at each iteration, which is a pain in the ass to track
- * separate string might fail only twice: at allocation and at writing
  */
-int	builtin_env(t_cmd *cmd)
+static size_t	env_get_len(void)
 {
-	size_t		len;
 	t_env_var	*cur;
-	char		*output;
+	size_t		len;
 
-	if (ft_strarrlen(cmd->argv) > 1)
-	{
-		ft_putstr_fd("minishell: env: too many agruments\n", STDERR_FILENO);
-		return (1);
-	}
 	len = 0;
 	cur = data(GET)->env.first;
 	while (cur)
@@ -39,9 +30,21 @@ int	builtin_env(t_cmd *cmd)
 			cur = cur->next;
 			continue ;
 		}
-		len += ft_strlen(cur->key) + ft_strlen(cur->value) + 2;
+		len += ft_strlen(cur->key) + 2;
+		if (cur->value)
+			len += ft_strlen(cur->value);
 		cur = cur->next;
 	}
+	return (len);
+}
+
+static char	*env_get_output(void)
+{
+	t_env_var	*cur;
+	char		*output;
+	size_t		len;
+
+	len = env_get_len();
 	output = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!output)
 		minishell_exit(1, NULL);
@@ -59,7 +62,25 @@ int	builtin_env(t_cmd *cmd)
 		ft_strlcat(output, "\n", len + 1);
 		cur = cur->next;
 	}
-	if (ft_putstr_fd(output, STDIN_FILENO) < 0)
+	return (output);
+}
+
+/**
+ * Why allocating a separate string for output while `printf` is availible?
+ * `printf` might fail at each iteration, which is a pain in the ass to track
+ * separate string might fail only twice: at allocation and at writing
+ */
+int	builtin_env(t_cmd *cmd)
+{
+	char		*output;
+
+	if (ft_strarrlen(cmd->argv) > 1)
+	{
+		ft_putstr_fd("minishell: env: too many agruments\n", STDERR_FILENO);
+		return (1);
+	}
+	output = env_get_output();
+	if (ft_putstr_fd(output, STDOUT_FILENO) < 0)
 		minishell_exit(1, NULL);
 	return (0);
 }
