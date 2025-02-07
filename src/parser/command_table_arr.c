68bb16d9407be267469a95dea11d84a3386e5478
GridisBlueFeathers <gridis72@gmail.com>
2025-02-07 14:33:24 +0100

    fix: fixed double free caused by ft_strjoin

--- src/parser/command_table_arr.c
+++ src/parser/command_table_arr.c
@@ -3,14 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   command_table_arr.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:47:56 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/01/27 15:12:39 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/02/07 13:08:15 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/string.h"
 #include "minishell.h"
+#include <stdio.h>
 
 void	ct_argv_set(t_prompt *prompt)
 {
@@ -40,6 +42,19 @@ void	ct_argv_set(t_prompt *prompt)
 	}
 }
 
+static void	ct_env_set_var(t_env_var *var, int i)
+{
+	size_t	len;
+
+	len = ft_strlen(var->key) +  ft_strlen(var->value) + 1;
+	data(GET)->env_arr[i] = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!data(GET)->env_arr[i])
+		minishell_exit(1, NULL);
+	ft_strlcat(data(GET)->env_arr[i], var->key, len + 1);
+	ft_strlcat(data(GET)->env_arr[i], "=", len + 1);
+	ft_strlcat(data(GET)->env_arr[i], var->value, len + 1);
+}
+
 void	ct_env_set(void)
 {
 	int			i;
@@ -55,15 +70,12 @@ void	ct_env_set(void)
 	i = 0;
 	while (env)
 	{
-		ms_data->env_arr[i] = ft_strdup(env->key);
-		if (!ms_data->env_arr[i])
-			minishell_exit(1, NULL);
-		ms_data->env_arr[i] = ft_strjoin(ms_data->env_arr[i], "=");
-		if (!ms_data->env_arr[i])
-			minishell_exit(1, NULL);
-		ms_data->env_arr[i] = ft_strjoin(ms_data->env_arr[i], env->value);
-		if (!ms_data->env_arr[i])
-			minishell_exit(1, NULL);
+		if (!env->value || env->was_unset)
+		{
+			env = env->next;
+			continue ;
+		}
+		ct_env_set_var(env, i);
 		i++;
 		env = env->next;
 	}
