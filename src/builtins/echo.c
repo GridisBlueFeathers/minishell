700e70bfdd45df04dcf5ab2fe6e400dc76332ae5
Julia Wolfram <julia.wolfram@gmx.at>
2025-02-11 12:08:00 +0100

    fix: echo prinnting extra space for last argument

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:39:13 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/02/10 17:30:14 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/02/11 11:53:20 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ int	builtin_echo(t_cmd *cmd)
 		i = 2;
 	while (cmd->argv[i])
 	{
-		check = printf("%s ", cmd->argv[i]);
+		if (!cmd->argv[i + 1])
+			check = printf("%s", cmd->argv[i]);
+		else
+			check = printf("%s ", cmd->argv[i]);
 		if (check < 0)
 			return (1);
 		i++;
