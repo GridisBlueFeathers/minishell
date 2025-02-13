bd995dac3542565319c34f107730903a75304523
Serhii Veretennikov <gridis72@gmail.com>
2025-02-13 15:42:36 +0100

    feat: implemented updating of PATH after env update

--- src/path.c
+++ src/path.c
@@ -6,20 +6,22 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:50:10 by jwolfram          #+#    #+#             */
-/*   Updated: 2024/11/27 15:25:30 by svereten         ###   ########.fr       */
+/*   Updated: 2025/02/13 14:21:54 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/stdlib.h"
 #include "minishell.h"
 
 void	path_set(void)
 {
 	t_env_var	*node;	
 
-	node = data(GET)->env.first;
-	while (ft_strcmp(node->key, "PATH"))
-		node = node->next;
-	data(GET)->path = ft_split(node->value, ':');
-	if (!data(GET)->path)
-		minishell_exit(1, NULL);
+	node = ft_getenv_node("PATH");
+	if (!node)
+		return ;
+	if (data(GET)->path)
+		ft_free(STR_ARR, &data(GET)->path);
+	if (node->value)
+		data(GET)->path = ft_split(node->value, ':');
 }
--- src/utils/is_builtin.c
+++ src/utils/is_builtin.c
@@ -6,7 +6,7 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 14:23:58 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/01/24 15:15:22 by jwolfram         ###   ########.fr       */
+/*   Updated: 2025/02/13 13:51:22 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	is_builtin(char *name)
 {
-	if (!ft_strncmp(name, "echo", ft_strlen(name))
-		|| !ft_strncmp(name, "cd", ft_strlen(name))
-		|| !ft_strncmp(name, "pwd", ft_strlen(name))
-		|| !ft_strncmp(name, "exit", ft_strlen(name))
-		|| !ft_strncmp(name, "env", ft_strlen(name))
-		|| !ft_strncmp(name, "export", ft_strlen(name))
-		|| !ft_strncmp(name, "unset", ft_strlen(name)))
+	if (!ft_strcmp(name, "echo")
+		|| !ft_strcmp(name, "cd")
+		|| !ft_strcmp(name, "pwd")
+		|| !ft_strcmp(name, "exit")
+		|| !ft_strcmp(name, "env")
+		|| !ft_strcmp(name, "export")
+		|| !ft_strcmp(name, "unset"))
 		return (1);
 	return (0);
 }
