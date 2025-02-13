883e40435fddbed7eba82dad07bccad369b7efea
Serhii Veretennikov <gridis72@gmail.com>
2025-02-13 15:45:11 +0100

    chore: added comment explaining path_set

--- src/path.c
+++ src/path.c
@@ -6,13 +6,17 @@
 /*   By: jwolfram <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:50:10 by jwolfram          #+#    #+#             */
-/*   Updated: 2025/02/13 14:21:54 by svereten         ###   ########.fr       */
+/*   Updated: 2025/02/13 15:44:42 by svereten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/stdlib.h"
 #include "minishell.h"
 
+/**
+ * If node is not present, then it was not present in env, so there is no
+ * data(GET)->path
+ */
 void	path_set(void)
 {
 	t_env_var	*node;	
