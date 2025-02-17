1084488f4a6aa223427fbc86c4d80d740402d496
Julia Wolfram <julia.wolfram@gmx.at>
2025-02-17 15:39:23 +0100

    fix(parser&&exit&&pwd): fixed more edgecases

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: svereten <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 16:14:10 by svereten          #+#    #+#             */
-/*   Updated: 2025/01/31 16:27:11 by svereten         ###   ########.fr       */
+/*   Updated: 2025/02/17 15:29:32 by jwolfram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -18,7 +18,7 @@ int	builtin_pwd(void)
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		minishell_exit(1, NULL);
-	if (printf("%s\n", cwd) < 0)
+	if (ft_putendl_fd(cwd, STDOUT_FILENO) < 0)
 		minishell_exit(1, NULL);
 	return (0);
 }
